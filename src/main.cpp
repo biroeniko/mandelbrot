926e323f397a7d5b861a63fb876ae562cf693887
Biro Eniko <encybiro@gmail.com>
2018-08-08 23:07:02 +0300

    CMakeLists.txt corrected. License corrected in c++ file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
-/* Copyright (C) 2016 Biro Eniko - All Rights Reserved
- * You may use, distribute and modify this code under the
- * terms of the license.
- * This file is subject to the terms and conditions defined in
- * file 'LICENSE.txt', which is part of this source code package.
- */
+/* MIT License
+
+Copyright (c) 2016 Biro Eniko
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 
 #include <iostream>
 #include <SDL2/SDL.h>
@@ -27,7 +44,7 @@ bool InitializeSDL()
     if (SDL_Init(SDL_INIT_VIDEO) == -1)
         return false;
 
-    window = SDL_CreateWindow("Fractal", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowRect.w, windowRect.h, 0);
+    window = SDL_CreateWindow("Mandelbrot", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowRect.w, windowRect.h, 0);
     if (!window)
         return false;
 
